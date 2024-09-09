ee0460f0c907d35d630578b9966ad9ecacf14f34
nijmra <armijn@gmail.com>
2024-09-09 09:51:04 +0200

    chore: changed formatting options - api-mock

--- odpc.client/mock/api.mock.ts
+++ odpc.client/mock/api.mock.ts
@@ -1,90 +1,90 @@
-import { type MockHandler } from 'vite-plugin-mock-server'
+import { type MockHandler } from "vite-plugin-mock-server";
 
 export const WAARDELIJSTEN = {
-  ORGANISATIE: 'Organisatie',
-  THEMA: 'Thema',
-  INFORMATIECATEGORIE: 'Informatiecategorie',
+  ORGANISATIE: "Organisatie",
+  THEMA: "Thema",
+  INFORMATIECATEGORIE: "Informatiecategorie"
   // DOCUMENTSOORT: 'Documentsoort'
-} as const
+} as const;
 
-type Waardelijst = keyof typeof WAARDELIJSTEN
+type Waardelijst = keyof typeof WAARDELIJSTEN;
 
 export type WaardelijstItem = {
-  id: number
-  type: Waardelijst
-  name: string
-  checked?: boolean
-}
+  id: number;
+  type: Waardelijst;
+  name: string;
+  checked?: boolean;
+};
 
 export type GroupedWaardeLijstItems = {
-  [key in Waardelijst]: WaardelijstItem[]
-}
+  [key in Waardelijst]: WaardelijstItem[];
+};
 
 const mocks: MockHandler[] = [
   {
-    pattern: '/api/waardelijsten',
+    pattern: "/api/waardelijsten",
     handle: (_req, res) => {
       const data: WaardelijstItem[] = [
         {
           id: 6,
-          type: 'THEMA',
-          name: 'arbeidsomstandigheden'
+          type: "THEMA",
+          name: "arbeidsomstandigheden"
         },
         {
           id: 8,
-          type: 'INFORMATIECATEGORIE',
-          name: 'wet of algemeen bindend voorschrift'
+          type: "INFORMATIECATEGORIE",
+          name: "wet of algemeen bindend voorschrift"
         },
         {
           id: 5,
-          type: 'THEMA',
-          name: 'afval'
+          type: "THEMA",
+          name: "afval"
         },
         {
           id: 2,
-          type: 'ORGANISATIE',
-          name: 'Organisatie 2'
+          type: "ORGANISATIE",
+          name: "Organisatie 2"
         },
         {
           id: 3,
-          type: 'ORGANISATIE',
-          name: 'Organisatie 3'
+          type: "ORGANISATIE",
+          name: "Organisatie 3"
         },
         {
           id: 4,
-          type: 'THEMA',
-          name: 'europese zaken'
+          type: "THEMA",
+          name: "europese zaken"
         },
         {
           id: 7,
-          type: 'THEMA',
-          name: 'arbeidsongeschikheid'
+          type: "THEMA",
+          name: "arbeidsongeschikheid"
         },
         {
           id: 1,
-          type: 'ORGANISATIE',
-          name: 'Organisatie 1'
+          type: "ORGANISATIE",
+          name: "Organisatie 1"
         }
-      ]
+      ];
 
-      res.setHeader('Content-Type', 'application/json')
+      res.setHeader("Content-Type", "application/json");
 
-      setTimeout(() => res.end(JSON.stringify(data)), 500)
+      setTimeout(() => res.end(JSON.stringify(data)), 500);
     }
   },
   {
-    pattern: '/api/gebruikersgroep/*',
-    method: 'PUT',
+    pattern: "/api/gebruikersgroep/*",
+    method: "PUT",
     handle: (req, res) => {
-      res.setHeader('Content-Type', 'application/json')
+      res.setHeader("Content-Type", "application/json");
 
-      req.on('data', (bodyString: string) => {
-        const body: object = JSON.parse(bodyString)
+      req.on("data", (bodyString: string) => {
+        const body: object = JSON.parse(bodyString);
 
-        setTimeout(() => res.end(JSON.stringify(body)), 500)
-      })
+        setTimeout(() => res.end(JSON.stringify(body)), 500);
+      });
     }
   }
-]
+];
 
-export default mocks
+export default mocks;
