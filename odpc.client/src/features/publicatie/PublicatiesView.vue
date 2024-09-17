8b1019b7f7686119adc420aeb1449e55d15b80d1
nijmra <armijn@gmail.com>
2024-09-17 17:25:50 +0200

    feat: publicaties mock api

--- odpc.client/src/features/publicatie/PublicatiesView.vue
+++ odpc.client/src/features/publicatie/PublicatiesView.vue
@@ -3,8 +3,12 @@
     <router-link :to="{ name: 'publicatie' }" class="button">Nieuwe publicatie</router-link>
   </p>
 
-  <ul>
-    <li v-for="{ identifier, officieleTitel, creatiedatum } in publicaties" :key="identifier">
+  <simple-spinner v-if="isFetching"></simple-spinner>
+
+  <alert-inline v-else-if="error">Er is iets misgegaan, probeer het nogmaals.</alert-inline>
+
+  <ul v-else>
+    <li v-for="{ identifier, officieleTitel, creatiedatum } in data" :key="identifier">
       <router-link :to="{ name: 'publicatie', params: { id: identifier } }">
         <h2>{{ officieleTitel }}</h2>
 
@@ -18,7 +22,12 @@
 </template>
 
 <script setup lang="ts">
-import { publicaties } from "../../../mock/api.mock";
+import { useFetchApi } from "@/api/use-fetch-api";
+import SimpleSpinner from "@/components/SimpleSpinner.vue";
+import AlertInline from "@/components/AlertInline.vue";
+import type { Publicatie } from "./types";
+
+const { data, isFetching, error } = useFetchApi("/api-mock/publicaties").json<Publicatie[]>();
 </script>
 
 <style lang="scss" scoped>
