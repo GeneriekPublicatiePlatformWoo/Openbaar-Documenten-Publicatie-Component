5b506cc79b8ed8ead1597cc94f3fcadf1f4a4dce
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-09-24 15:11:31 +0200

    chore: backend mocks voor documenten en formats

---
+++ ODPC.Server/Features/Formats/FormatsOverzicht/FormatsOverzichtController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ODPC.Features.Formats.FormatsOverzicht
+{
+    [ApiController]
+    public class FormatsOverzichtController : ControllerBase
+    {
+        [HttpGet("/api/v1/formats")]
+        public IActionResult Get()
+        {
+            return Ok(FormatsMock.Formats.Values.OrderBy(x => x.Name));
+        }
+    }
+}
