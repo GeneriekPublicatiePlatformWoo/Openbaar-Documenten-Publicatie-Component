782b55d4ba9190198322ff081e91f20c2dfd3090
mark stoker <mark@icatt.nl>
2024-10-14 17:04:24 +0200

    opzet infocategorien uit odrc

--- ODPC.Server/Features/WaardeLijstenOverzicht/WaardelijstViewModel.cs
+++ ODPC.Server/Features/WaardeLijstenOverzicht/WaardelijstViewModel.cs
@@ -1,10 +1,33 @@
-﻿namespace ODPC.Features.WaardeLijstenOverzicht
-{
-    public class WaardelijstViewModel
-    {
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
 
+namespace ODPC.Features.WaardeLijstenOverzicht
+{
+    public  class WaardelijstViewModel
+    {
+        
         public required string Id { get; set; }
+    
         public required string Name { get; set; }
-        public required string Type { get; set; }
+
+   
+        public string Type { get; set; } = "INFORMATIECATEGORIE";
     }
+
+
+    //public class InformatiecategorieViewModel : WaardelijstViewModel
+    //{     
+    //    public override string Type => "INFORMATIECATEGORIE";
+    //}
+
+    //public class ThemaViewModel : WaardelijstViewModel
+    //{
+    //    public override string Type => "THEMA";
+    //}
+
+    //public class OrganisatieViewModel : WaardelijstViewModel
+    //{
+    //    public override string Type => "ORGANISATIE";
+    //} 
+
 }
--- ODPC.Server/Features/WaardeLijstenOverzicht/WaardelijstenController.cs
+++ ODPC.Server/Features/WaardeLijstenOverzicht/WaardelijstenController.cs
@@ -1,24 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ODPC.Apis.Odrc;
+using ODPC.Authentication;
+using ODPC.Config;
+using ODPC.Features.Publicaties;
 
 namespace ODPC.Features.WaardeLijstenOverzicht
 {
     [Route("api/v1/[controller]")]
     [ApiController]
-    public class WaardelijstenController : ControllerBase
+    public class WaardelijstenController(OdpUser user, IOrdcClientFactory clientFactory) : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<WaardelijstViewModel> Get()
+        public async Task<IEnumerable<WaardelijstViewModel>> GetAsync()
         {
-            //hoe de waardelijsten api eruit komt te zien is nog onbekend. mogelijk wordt dit tzt losse endpoints per soort lijst, mogelijk worden de id's url's, mogelijk verschilt het model per soort lijst....
-            //gezien alle onbekendheden kiezen we even voor een zo simpel mogelijke start opzet
-            return new WaardelijstViewModel[] {
-                new() { Id = "d3da5277-ea07-4921-97b8-e9a181390c76", Name="arbeidsomstandigheden", Type="THEMA" },
-                new() { Id = "f1e4632d-9c71-4ea4-9048-a5665671bbe4", Name="wet of algemeen bindend voorschrift", Type="INFORMATIECATEGORIE" },
-                new() { Id = "066241fe-7f39-41da-8efb-a702ef32b7d0", Name = "afval", Type="THEMA" },
-                new() { Id = "8f939b51-dad3-436d-a5fa-495b42317d64", Name = "Organisatie 2", Type="ORGANISATIE" },
-                new() { Id = "5c14e7e2-00a2-4990-adbb-7290cd89fb6e", Name = "Organisatie 3", Type="ORGANISATIE" },
-                new() { Id = "0e7a0023-423a-421a-8700-359232fef584", Name = "europese zaken", Type="THEMA" }
-            };
+            // Deserialization of types without a parameterless constructor, a singular parameterized constructor, or a parameterized constructor annotated with 'JsonConstructorAttribute' is not supported. Type 'ODPC.Features.WaardeLijstenOverzicht.WaardelijstViewModel'.
+
+            //infocategorien ophalen uit het ODRC
+
+            var client = clientFactory.Create(user, "Waardelijsten ophalen");
+
+            var response = await client.GetAsync("/api/v1/informatiecategorieen/", new CancellationToken());
+
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            var deserializedResponse = JsonSerializer.Deserialize<PagedResponseModel<WaardelijstResponseModel>>(responseBody, JsonSerialization.Options);
+
+            var results = deserializedResponse?.results ?? [];
+
+            //alle wardelijsten zitten in 1 response. wellicht splitsten?
+            //op dit moment worden alleen de inormatiecageroien opgehaald. voor nu aanvullen met wat mock data
+            //viewModel.Add(new ThemaViewModel() { Id = "d3da5277-ea07-4921-97b8-e9a181390c76", Name = "arbeidsomstandigheden" });
+            //viewModel.Add(new ThemaViewModel() { Id = "066241fe-7f39-41da-8efb-a702ef32b7d0", Name = "afval"  });
+            //viewModel.Add(new OrganisatieViewModel() { Id = "8f939b51-dad3-436d-a5fa-495b42317d64", Name = "Organisatie 2"});
+            //viewModel.Add(new OrganisatieViewModel () { Id = "5c14e7e2-00a2-4990-adbb-7290cd89fb6e", Name = "Organisatie 3" });
+            //viewModel.Add(new ThemaViewModel() { Id = "0e7a0023-423a-421a-8700-359232fef584", Name = "europese zaken" });
+
+            var viewModel = results.Select(x=> new WaardelijstViewModel { Id = x.Id, Name = x.Name}).ToList();
+
+
+
+            viewModel.Add(new WaardelijstViewModel() { Id = "d3da5277-ea07-4921-97b8-e9a181390c76", Name = "arbeidsomstandigheden", Type = "THEMA" });
+            viewModel.Add(new WaardelijstViewModel() { Id = "066241fe-7f39-41da-8efb-a702ef32b7d0", Name = "afval", Type = "THEMA" });
+
+            viewModel.Add(new WaardelijstViewModel() { Id = "8f939b51-dad3-436d-a5fa-495b42317d64", Name = "Organisatie 2", Type="ORGANISATIE" });
+            viewModel.Add(new WaardelijstViewModel() { Id = "5c14e7e2-00a2-4990-adbb-7290cd89fb6e", Name = "Organisatie 3", Type = "ORGANISATIE" });
+            viewModel.Add(new WaardelijstViewModel() { Id = "0e7a0023-423a-421a-8700-359232fef584", Name = "europese zaken", Type = "THEMA" });
+            return viewModel;
         }
+
+
     }
 }
