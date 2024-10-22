47d6552511844eb6ddedc9a9563f3703a3fa91d2
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-10-22 13:06:17 +0200

    validatie op informatiecategorieen

--- ODPC.Server/Features/Informatiecategorieen/MijnInformatiecategorieen/MijnInformatiecategorieenController.cs
+++ ODPC.Server/Features/Informatiecategorieen/MijnInformatiecategorieen/MijnInformatiecategorieenController.cs
@@ -3,31 +3,24 @@ using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ODPC.Apis.Odrc;
-using ODPC.Authentication;
-using ODPC.Data;
 
 namespace ODPC.Features.Informatiecategorieen.MijnInformatiecategorieen
 {
     [ApiController]
-    public class MijnInformatiecategorieenController(OdpcUser user, IOdrcClientFactory clientFactory, OdpcDbContext context) : ControllerBase
+    public class MijnInformatiecategorieenController(IOdrcClientFactory clientFactory, IGebruikerWaardelijstItemsService waardelijstItemsService) : ControllerBase
     {
         [HttpGet("api/v1/mijn-informatiecategorieen")]
         public async IAsyncEnumerable<JsonObject> Get([EnumeratorCancellation] CancellationToken token)
         {
-            var groepIds = context.GebruikersgroepGebruikers
-                .Where(x => x.GebruikerId == user.Id)
-                .Select(x => x.GebruikersgroepUuid);
-
-            var categorieen = await context.GebruikersgroepWaardelijsten
-                .Where(x => groepIds.Contains(x.GebruikersgroepUuid))
-                .Select(x => x.WaardelijstId)
-                .Distinct()
-                .ToListAsync(token);
+            var categorieen = await waardelijstItemsService.GetAsync(token);
 
             if (categorieen.Count == 0) yield break;
 
-            using var client = clientFactory.Create(user, "Eigen informatiecategorieen ophalen");
+            using var client = clientFactory.Create("Eigen informatiecategorieen ophalen");
             var url = "api/v1/informatiecategorieen";
+
+            // omdat we zelf moeten filteren obv van de waardelijstitems waar de gebruiker toegang toe heeft,
+            // kunnen we geen paginering gebruiker. we lopen door alle pagina's van de ODRC
             while (!string.IsNullOrWhiteSpace(url))
             {
                 var page = await client.GetFromJsonAsync<PagedResponseModel<JsonObject>>(url, token) ?? new() { Results = [] };
