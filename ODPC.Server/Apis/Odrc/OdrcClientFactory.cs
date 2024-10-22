add1d907ee7965d86942edacd4e03b6b37da5cbe
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-10-22 10:21:04 +0200

    fix: audit headers

--- ODPC.Server/Apis/Odrc/OdrcClientFactory.cs
+++ ODPC.Server/Apis/Odrc/OdrcClientFactory.cs
@@ -20,9 +20,9 @@ namespace ODPC.Apis.Odrc
             var client = _httpClientFactory.CreateClient();
             client.BaseAddress = new(config.BaseUrl);
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", config.ApiKey); //dit doet nog niets. nog niet geimplementeerd aan odrc kant. nb nog niet duidelijk of dit de juiste manier zal zijn voor het meesturen van het token
-            client.DefaultRequestHeaders.Add("Audit-User-Id", user.Id); //dit doet nog niets. nog niet geimplementeerd aan odrc kant
-            client.DefaultRequestHeaders.Add("Audit-User-Display-Name", user.FullName); //dit doet nog niets. nog niet geimplementeerd aan odrc kant
-            client.DefaultRequestHeaders.Add("Audit-Toelichting", handeling); //dit doet nog niets. nog niet geimplementeerd aan odrc kant
+            client.DefaultRequestHeaders.Add("Audit-User-ID", user.Id);
+            client.DefaultRequestHeaders.Add("Audit-User-Representation", user.FullName);
+            client.DefaultRequestHeaders.Add("Audit-Remarks", handeling);
             return client;
         }
 
