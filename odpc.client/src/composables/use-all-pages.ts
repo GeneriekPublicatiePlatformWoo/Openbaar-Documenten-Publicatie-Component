a0c32d0ab426a30b4ee04da9b7b403492362702c
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-10-29 10:09:43 +0100

    cleanup

--- odpc.client/src/composables/use-all-pages.ts
+++ odpc.client/src/composables/use-all-pages.ts
@@ -3,9 +3,9 @@ import { asyncComputed } from "@vueuse/core";
 import { ref } from "vue";
 
 const fetchPage = <T>(url: string, signal?: AbortSignal | undefined) =>
-  fetch(url, { headers: { "is-api": "true" }, signal }).then(
-    (r) => r.json() as Promise<PagedResult<T>>
-  );
+  fetch(url, { headers: { "is-api": "true" }, signal })
+    .then((r) => (r.ok ? r : Promise.reject(r.status)))
+    .then((r) => r.json() as Promise<PagedResult<T>>);
 
 const fetchAllPages = async <T>(url: string, signal?: AbortSignal | undefined): Promise<T[]> => {
   const { results, next } = await fetchPage<T>(url, signal);
