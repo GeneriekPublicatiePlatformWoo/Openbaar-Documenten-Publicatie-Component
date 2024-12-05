92195414807254517f23529fda1066296f097b0b
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-12-05 09:43:14 +0100

    theming

--- docs/conf.py
+++ docs/conf.py
@@ -51,13 +51,16 @@ epub_show_urls = "footnote"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_logo = "logo.png"
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = [
+    "theme_overrides.css",  # override wide tables with word wrap by Maykin
+]
